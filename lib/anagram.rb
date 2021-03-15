f486f1dd04833a3de9725ff1ad28e7c062bef71e
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-15 21:10:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+     @word = word
+   end
+
+   def match(string)
+     string.select do |word| word.split("").sort == word.split("").sort
+end
+end
+end
+#some_word.split("") == word.split("")
+#  word.match(%w(some_word))
